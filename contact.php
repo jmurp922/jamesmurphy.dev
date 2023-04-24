98119bce0dc7f81ff075958162ed1c4394c2178c
James Murphy <jmurp922@gmail.com>
2023-04-24 16:53:59 -0400

    Update php file to add subject

--- contact.php
+++ contact.php
@@ -1,18 +1,12 @@
 <?php  
-    $errors = '';
     $myemail = 'email@jamesmurphy.dev';
-
     $name = $_POST['name']; 
     $email_address = $_POST['email']; 
     $message = $_POST['message']; 
     $email_subject = $_POST['subject'];
-
     $to = $myemail;
-
-    $email_body = "You have received a new message from ". $name . "\nEmail: $email_address\n Message \n $message";
-
+    $email_body = "You have received a new message from ". $name . "\nEmail: $email_address\nSubject: $email_subject \nMessage \n $message";
     $headers = "From: $myemail\n" . "Reply-To: $email_address";
     mail($to,$email_subject,$email_body,$headers);
     header("Location: .");
-
 ?>
